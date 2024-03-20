dac6b8bcc6dfd8e17bd90311ed922b49aacb964b
lu7r4 <lu7r4@lu7r4s-MacBook-Air.local>
2024-03-20 21:42:26 +0300

    remove usename, edit btns

--- create_bot.py
+++ create_bot.py
@@ -7,13 +7,3 @@ bot = Bot(token=token_bot)
 
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
-
-"""
-import os
-token_bot = os.environ.get('TOKEN_BOT')
-bot = Bot(token=token_bot)
-
-
-bot_token = "6743399707:AAEi4LsoMUad2eRpR4cGt1s4G8TGNINvk8o"
-bot = Bot(token=bot_token)
-"""
--- handlers/fsm.py
+++ handlers/fsm.py
@@ -21,13 +21,9 @@ from aiocron import crontab
 import os
 import requests
 
-
-# Создаем соединение с базой данных Redis
-
-# Объявляем класс состояний
+# env
 redis_host = os.environ.get('REDIS_HOST')
 redis_port = os.environ.get('REDIS_PORT')
-
 
 class StartState(StatesGroup):
     STARTED = State()
@@ -38,56 +34,54 @@ class PingState(StatesGroup):
     ActionChoose = State()
     RepeatAction = State()
 
-# Обрабатываем команду /start
+# cmd /start
 async def cmd_start(message: types.Message, state: FSMContext):
-    # Отправляем сообщение с приветствием и клавиатурой
+
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     bd_btn = types.KeyboardButton("Monitoring IPs")
     test_btn = types.KeyboardButton("Test the network")
     keyboard.add(test_btn, bd_btn)
     
     await message.answer( f"Shalom, *{message['from'].first_name}*!\nPlease press the button to Test the network or Monitoring IPs.", reply_markup=keyboard)
+    
     chat_id = message.chat.id
     asyncio.create_task(ping_urls_periodically(state, chat_id))
-    # Возвращаемся в начальное состояние
     await state.finish()
 
-    # Переходим в состояние STARTED
     await StartState.STARTED.set()
 
-# Обрабатываем команду /начало
+# btn "to the begining"
 @dp.message_handler(lambda message: message.text == "to the begining", state='*')
 async def bnt_start(message: types.Message, state: FSMContext):
-    # Отправляем сообщение с приветствием и клавиатурой
+
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     bd_btn = types.KeyboardButton("Monitoring IPs")
     test_btn = types.KeyboardButton("Test the network")
     keyboard.add(test_btn, bd_btn)
     
     await message.answer( f"*{message['from'].first_name}*!\nPlease press the button to Test the network or Monitoring IPs.", reply_markup=keyboard)
+    
     chat_id = message.chat.id
     asyncio.create_task(ping_urls_periodically(state, chat_id))
-    # Возвращаемся в начальное состояние
+
     await state.finish()
 
-    # Переходим в состояние STARTED
     await StartState.STARTED.set()    
 
+# monitoring
 @dp.message_handler(lambda message: message.text == "Monitoring IPs", state='*')
 async def db_handler(message: types.Message, state: FSMContext):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True) 
 
-    keyboard.row('View your IPs', 'Add IP', 'Remove IP')
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True) 
+    keyboard.row('View IPs', 'Add IP', 'Remove IP')
     keyboard.row('to the begining')
 
-    await message.answer(f"DB for *{message['from'].first_name}*", reply_markup=keyboard)
+    await message.answer("Choose an action for", reply_markup=keyboard)
 
-
-
-
+# address verification
 @dp.message_handler(lambda message: message.text in ["Test the network", "Add IP", "Remove IP"], state='*')
 async def cmd_handler(message: types.Message, state: FSMContext):
-    await state.update_data(selected_button=message.text)  # Сохраняем выбранную кнопку в состоянии
+    await state.update_data(selected_button=message.text)  
     await message.answer("Enter URL website", reply_markup=types.ReplyKeyboardRemove())
     await PingState.EnterURL.set()
 
@@ -98,7 +92,6 @@ async def enter_url(message: types.Message, state: FSMContext):
     url = url.lower()
 
     await state.update_data(url=url)
-    await state.update_data(username=message.from_user.first_name)  # Сохраняем username в состоянии
 
     try:
         socket.inet_aton(url)
@@ -120,10 +113,10 @@ async def enter_url(message: types.Message, state: FSMContext):
         keyboard.row('to the begining')
 
     elif selected_button == "Add IP":
-        keyboard.add(KeyboardButton(text="Add your IP to DB"))
+        keyboard.add(KeyboardButton(text="Add IP to DB"))
 
     elif selected_button == "Remove IP":
-        keyboard.add(KeyboardButton(text="Remove your IP from DB"))
+        keyboard.add(KeyboardButton(text="Remove IP from DB"))
 
     if selected_button == "Test the network":
         await message.answer(
@@ -142,15 +135,13 @@ async def enter_url(message: types.Message, state: FSMContext):
             reply_markup=keyboard
         )
 
-
     await PingState.ActionChoose.set()
 
-
-@dp.message_handler(lambda message: message.text == "Add your IP to DB" or message.text == "Remove your IP from DB" or message.text == "Ping" or message.text == "Nmap" or message.text == "Traceroute" or message.text == "Curl", state=[PingState.ActionChoose, PingState.RepeatAction])
+# btns
+@dp.message_handler(lambda message: message.text == "Add IP to DB" or message.text == "Remove IP from DB" or message.text == "Ping" or message.text == "Nmap" or message.text == "Traceroute" or message.text == "Curl", state=[PingState.ActionChoose, PingState.RepeatAction])
 async def ping_chosen_handler(message: types.Message, state: FSMContext):
     data = await state.get_data()
     url = data.get("url", None)
-    username = data.get("username")  # Получаем username из состояния
 
     if message.text == "Ping":
         result = await ping_website_function(url)
@@ -171,28 +162,18 @@ async def ping_chosen_handler(message: types.Message, state: FSMContext):
     elif message.text == "Curl":
         result = await curl_command(url, message)
         await message.answer(result)
-    elif message.text == "Add your IP to DB":
-        if username:
-            await store_url(url, username, message)  # Вызываем функцию store_url() и передаем переменную url
-        else:
-            await message.answer("Error: User ID not found.")
-            return
-            
-        await state.reset_state()  # Сбрасываем состояние
-    elif message.text == "Remove your IP from DB":
-        if username:
-            await remove_url(url, username, message)  # Вызываем функцию store_url() и передаем переменную url
-        else:
-            await message.answer("Error: User ID not found.")
-            return
-            
-        await state.reset_state()  # Сбрасываем состояние
+    elif message.text == "Add IP to DB":
+        await store_url(url, message)  
+        await state.reset_state()  
+    elif message.text == "Remove IP from DB":
+        await remove_url(url, message)  
 
+        await state.reset_state()  
 
-@dp.message_handler(lambda message: message.text == "View your IPs", state='*')
+# db
+@dp.message_handler(lambda message: message.text == "View IPs", state='*')
 async def db_list_handler(message: types.Message, state: FSMContext):
     data = await state.get_data()
-    username = data.get("username")  # Получаем username из состояния
     redis_connection = redis.Redis(host=redis_host, port=redis_port)
     all_keys = redis_connection.keys()
 
@@ -204,16 +185,12 @@ async def db_list_handler(message: types.Message, state: FSMContext):
             db_list.append(value.decode())
 
     if db_list:
-        response = "Your database addresses:\n"
+        response = "Addresses in the DB:\n"
         for ip in db_list:
             response += f"- {ip}\n"
         await message.answer(response)
     else:
         await message.answer("Your database is empty.")
-
-async def ping_website_function(url):    
-    response_time = ping3.ping(url)
-    return f"Время отклика {url}: {response_time} мс"
 
 # stop traceroute
 @dp.message_handler(lambda message: message.text == "Stop", state=[PingState.ActionChoose, PingState.RepeatAction])
@@ -228,8 +205,12 @@ async def stop_trace_route_handler(callback_query: types.CallbackQuery):
     chat_id = callback_query.message.chat.id
     await stop_trace_route(callback_query, chat_id, bot)
 
-async def nmap_function(url, chat_id, bot, message):
+# ping
+async def ping_website_function(url):    
+    response_time = ping3.ping(url)
+    return f"Время отклика {url}: {response_time} мс"
 
+async def nmap_function(url, chat_id, bot, message):
     nm = nmap.PortScanner()
     try:
         for i in range(1, 500, 50):
@@ -265,8 +246,7 @@ async def nmap_function(url, chat_id, bot, message):
         print(f"Error: {str(e)}")
         await bot.send_message(chat_id=chat_id, text='An error occurred')
 
-#    await message.answer(f"Выберите действие для {domain_name}:")
-
+# traceroute
 async def trace_route_website_function(url, chat_id, bot):
     global stop_traceroute, process
     try:
@@ -284,7 +264,6 @@ async def trace_route_website_function(url, chat_id, bot):
             if not line:
                 break
 
-            # Отправить сообщение по строчно в бота
             message = line.decode('utf-8')
             # Создание кнопки "Stop"
             stop_button = types.KeyboardButton("Stop")
@@ -300,6 +279,7 @@ async def trace_route_website_function(url, chat_id, bot):
     except Exception as e:
         await bot.send_message(chat_id, f"An error occurred while performing the traceroute: {str(e)}")
 
+# curl
 async def curl_command(url, message):
     try:
         ip = socket.gethostbyname(url)
@@ -318,55 +298,48 @@ async def curl_command(url, message):
     except Exception as e:
         await message.answer(f"The curl command encountered an error: {str(e)}")
                 
-"""
-async def curl_command(url, message):
-    ip = socket.gethostbyname(url)
-    
-    # Выполняем команду curl для получения содержимого страницы
-    result = subprocess.check_output(["curl", ip]).decode("utf-8")
-    
-    await message.answer(f"Для {ip} \n\n The result of the curl command:\n{result}")
-""" 
-async def store_url(url, username, message):
-    # Устанавливаем соединение с базой данных Redis
+
+# btn Add IP to DB
+async def store_url(url, message):
+
     redis_connection = redis.Redis(host=redis_host, port=redis_port)
-    key = f"{username}:{url}"
+
+    key = f"{url}"
     stored_url = redis_connection.get(key)
 
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True) 
 
-    keyboard.row('View your IPs', 'Add IP', 'Remove IP')
+    keyboard.row('View IPs', 'Add IP', 'Remove IP')
     keyboard.row('to the begining')
 
     if stored_url is not None:
         stored_url = stored_url.decode()  # Преобразуем байтовую строку в обычную строку
-        await message.answer(f"URL {stored_url} already exists in the database for user {username}.", reply_markup=keyboard)
+        await message.answer(f"URL {stored_url} already exists in the DB.", reply_markup=keyboard)
     else:
         redis_connection.set(key, url)
-        await message.answer(f"URL {url} has been successfully added to the database!", reply_markup=keyboard)
+        await message.answer(f"URL {url} has been successfully added to the DB!", reply_markup=keyboard)
 
-async def remove_url(url, username, message):
-    # Устанавливаем соединение с базой данных Redis
+# btn Remove IP from DB
+async def remove_url(url, message):
     redis_connection = redis.Redis(host=redis_host, port=redis_port)
-    key = f"{username}:{url}"
+    key = f"{url}"
     remove_url = redis_connection.get(key)
 
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True) 
-
-    keyboard.row('View your IPs', 'Add IP', 'Remove IP')
+    keyboard.row('View IPs', 'Add IP', 'Remove IP')
     keyboard.row('to the begining')
 
     if remove_url is not None:
         remove_url = remove_url.decode()  # Преобразуем байтовую строку в обычную строку
         redis_connection.delete(key)
-        await message.answer(f"URL {url} has been successfully removed from the database for user {username}.", reply_markup=keyboard)
+        await message.answer(f"URL {url} has been successfully removed from the DB.", reply_markup=keyboard)
     else:
-        await message.answer(f"URL {url} does not exist in the database for user {username}.", reply_markup=keyboard)
+        await message.answer(f"URL {url} has been successfully removed from the DB.", reply_markup=keyboard)
 
+# btn View your DB
 @dp.message_handler(lambda message: message.text == "View your DB", state='*')
 async def db_list_handler(message: types.Message, state: FSMContext):
     data = await state.get_data()
-    username = data.get("username")  # Получаем username из состояния
     redis_connection = redis.Redis(host=redis_host, port=redis_port)
     all_keys = redis_connection.keys()
 
@@ -378,58 +351,14 @@ async def db_list_handler(message: types.Message, state: FSMContext):
             db_list.append(value.decode())
 
     if db_list:
-        response = "Your database addresses:\n"
+        response = "Addresses in the DB:\n"
         for ip in db_list:
             response += f"- {ip}\n"
         await message.answer(response)
     else:
         await message.answer("Your database is empty.")
 
-"""
-@dp.message_handler(lambda message: message.text == "View your DB")
-async def db_list_handler(message: types.Message, state: FSMContext):
-    data = await state.get_data()
-    username = data.get("username")  # Получаем username из состояния
-    redis_connection = redis.Redis(host=redis_host, port=redis_port)
-    all_keys = redis_connection.keys()
-
-
-    db_list = []
-
-    for key in all_keys:
-        value = redis_connection.get(key)
-        if value is not None:
-            db_list.append(value.decode())
-    
-    if db_list:
-        keyboard = InlineKeyboardMarkup()
-
-        for ip in db_list:
-            keyboard.add(InlineKeyboardButton(text=ip, callback_data='ip_' + ip))
-            print(ip)
-            print('ip_' + ip)
-
-        # Вывод кнопок в консоль
-        for button in keyboard.inline_keyboard:
-            print(button[0].text)
-
-        await message.answer("Choose an action for the IP:", reply_markup=keyboard)
-    else:
-        await message.answer("Your database is empty.")
-
-
-
-@dp.callback_query_handler(lambda c: c.data.startswith('ip_'))
-async def db_callback_test(callback: types.CallbackQuery):
-    ip = callback.data.split('_')[1]
-
-    keyboard = types.InlineKeyboardMarkup(row_width=2)
-    delete_button = types.InlineKeyboardButton("Delete", callback_data=f"delete_{ip}")
-    test_button = types.InlineKeyboardButton("Test Network", callback_data=f"test_{ip}")
-    keyboard.add(delete_button, test_button)
-    
-    await bot.send_message(callback.message.chat.id, f"You clicked on IP: {ip}")
-"""
+# ping 
 async def ping_urls_periodically(state, chat_id):
 
     unavailable_ips = []  # Список для хранения недоступных IP-адресов
@@ -437,7 +366,6 @@ async def ping_urls_periodically(state, chat_id):
 
     while True:
         data = await state.get_data()
-        username = data.get("username")  # Получаем username из состояния
         redis_connection = redis.Redis(host=redis_host, port=redis_port)
         all_keys = redis_connection.keys()
 
@@ -450,14 +378,14 @@ async def ping_urls_periodically(state, chat_id):
                 if response_time is None:
                     if ip not in unavailable_ips:
                         if len(unavailable_ips) < 3:
-                            message = f"IP {ip} не доступен"
+                            message = f"IP {ip} is not available."
                             await bot.send_message(chat_id=chat_id, text=message)
                         unavailable_ips.append(ip)
                 else:
                     if ip in unavailable_ips:
                         unavailable_ips.remove(ip)
                         available_ips.append(ip)
-                        message = f"IP {ip} стал доступен"
+                        message = f"IP {ip} has become available."
                         await bot.send_message(chat_id=chat_id, text=message)
 
         await asyncio.sleep(5)  # Пауза 5 секунд перед следующим пингом
@@ -472,21 +400,11 @@ def register_handlers_fsm(dp: Dispatcher):
     dp.register_message_handler(cmd_handler, lambda message: message.text in ["Test the network", "Add IP", "Remove IP"], state='*')
     dp.register_message_handler(db_handler, lambda message: message.text == "Monitoring IPs", state='*')
     dp.register_message_handler(enter_url, state=PingState.EnterURL)
-    dp.register_message_handler(ping_chosen_handler, lambda message: message.text == "Ping" or message.text == "Add your IP to DB" or message.text == "Remove your IP from DB" or message.text == "Nmap" or message.text == "Traceroute" or message.text == "Curl", state=[PingState.ActionChoose, PingState.RepeatAction])
-    dp.register_message_handler(db_list_handler, lambda message: message.text == "View your IPs", state='*')
+    dp.register_message_handler(ping_chosen_handler, lambda message: message.text == "Ping" or message.text == "Add IP to DB" or message.text == "Remove IP from DB" or message.text == "Nmap" or message.text == "Traceroute" or message.text == "Curl", state=[PingState.ActionChoose, PingState.RepeatAction])
+    dp.register_message_handler(db_list_handler, lambda message: message.text == "View IPs", state='*')
     dp.register_message_handler(stop_traceroute_handler,  lambda message: message.text == "Stop", state=[PingState.ActionChoose, PingState.RepeatAction])
-    
-#    dp.register_callback_query_handler(db_callback_test, lambda c: c.data.startswith('ip_'))
     dp.register_callback_query_handler(stop_trace_route_handler, lambda c: c.data == 'stop')
-
     dp.register_message_handler(bnt_start, lambda message: message.text == "to the begining", state='*')
-
-
-
-
-#    dp.register_message_handler(test_cmd, Command("test"))
-#    dp.register_callback_query_handler(db_callback_handler)
-#    dp.register_callback_query_handler(db_callback_test)
 
 
 
